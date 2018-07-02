d55dc1163ccd4d85ecfb96441da998c00a91f8e7
Nastia <fomina.nst@gmail.com>
2018-07-03 02:18:04 +0300

    correct full name

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,6 +5,12 @@ class Client < ApplicationRecord
                       :length => { :minimum => 6, :maximum => 10 }
 
     def full_name
-        "#{name} #{last_name}"
+        if name.present? && last_name.present?
+            name + " " + last_name
+        elsif name.blank? && last_name.blank?
+            ""
+        else name.blank? || last_name.blank?
+            name.presence || last_name.presence
+        end
     end
 end
